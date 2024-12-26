3ee69c93fb7a4036561540bad96c5c6e7359389d
DarshanBothra <66417823+DarshanBothra@users.noreply.github.com>
2024-12-27 03:39:00 +0530

    Update network.py

--- network.py
+++ network.py
@@ -10,7 +10,6 @@ class Network:
     def __init__(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server = "<LOCALHOST SERVER IP HERE>"
-        self.server = "127.0.0.1"
         self.port = 5555
         self.addr = (self.server, self.port)
         self.p = self.connect()
